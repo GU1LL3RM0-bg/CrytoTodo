44cc55905886aa3a81420a39e5b664c83c5d7332
guillermo <guillermo>
2021-02-19 19:22:58 -0500

    Complete la parte 3 y 4 de la encriptacion y desencriptacion asimetrica con RSA usando el modulo RSA de Crypto.PublicKey pd: burlado

--- elmago.py
+++ elmago.py
@@ -1,6 +1,11 @@
 import cryptography
 from cryptography.fernet import Fernet
 
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+import base64
+
 
 # generar key solamente en un archivo
 def generateKeyWithoutPassword(keyName):
@@ -47,7 +52,7 @@ def decryptionMethod(file2decrypt,llave):
         f.close()
 
 def menu():
-    print("############### BIENVENID@ ###############")
+    print("\n############### BIENVENID@ ###############\n")
     print("Elija la opción deseada:")
     print("\t 1 - Encriptacion Simetrica")
     print("\t 2 - Desencriptacion Simetrica")
@@ -57,12 +62,68 @@ def menu():
     print("\t 6 - Cifrado por Matrices")
     selection = input("\n -->: ")
     return int(selection)
+
+def generatePublicAndPrivateKeys():
+    # Generates RSA Encryption + Decryption keys / Public + Private keys
+    key = RSA.generate(2048)
+
+    private_key = key.export_key()
+    with open('private.pem', 'wb') as f:
+        f.write(private_key)
+        f.close()
+
+    public_key = key.publickey().export_key()
+    with open('public.pem', 'wb') as f:
+        f.write(public_key)
+        f.close()
+
+
+def encryptionMethodAsymetric(fileName):
+    try:
+        with open(fileName, 'rb') as fn:
+            archivo = fn.read()
+
+        with open(fileName, 'wb') as f:
+            # Public RSA key
+            public_key = RSA.import_key(open('public.pem').read())
+            # Public encrypter object
+            public_crypter =  PKCS1_OAEP.new(public_key)
+            # Encrypted fernet key
+            file_encrypted = public_crypter.encrypt(archivo)
+            # Write encrypted fernet key to file
+            f.write(file_encrypted)
+            f.close()
+        print("Archivo encriptado exitosamente. Protege la llave privada private.pem !!")
+    except ValueError as e:
+        print("Error técnico." + str(e))
+
+def decryptionMethodAsymetric(file, llave):
+    try:
+        with open(file, 'rb') as f:
+            archivo_encriptado = f.read()
+        # Private RSA key
+        private_key = RSA.import_key(open(llave).read())
+        # Private decrypter
+        private_crypter = PKCS1_OAEP.new(private_key)
+        # Decrypted session key
+        archivo_desencriptado = private_crypter.decrypt(archivo_encriptado)
+        with open(file, 'wb') as f:
+            f.write(archivo_desencriptado)
+
+        print('Archivo desencriptado exitosamente!')
+    except ValueError as e:
+        print("Error Tecnico: " + str(e))
+
 def main():
     opt  = menu()
     if opt == 1:
         try:
-            name = input("\nIngrese nombre de su nueva llave secreta: " )
-            generateKeyWithoutPassword(name)
+            si_no = input("\nDesea generar una llave nueva?... escriba si o no: ")
+            if si_no == "si":
+                name = input("\nIngrese nombre de su nueva llave secreta: " )
+                generateKeyWithoutPassword(name)
+            else:
+                print("\n")
             file = input("\nIngrese el nombre del archivo a encriptar incluyendo la extension: ")
             key = input("\nIngrese el nombre de su llave incluyendo la extension: ")
             encryptionMethod(file,key)
@@ -79,6 +140,20 @@ def main():
         except ValueError as e:
             print(e)
 
+    if opt == 3:
+        try:
+            file = input("\nIngrese el nombre del archivo a encriptar incluyendo la extension: ")
+            generatePublicAndPrivateKeys()
+            encryptionMethodAsymetric(file)
+        except ValueError as e:
+            print(e)
+    if opt == 4:
+        try:
+            file = input("\nIngrese el nombre del archivo a desencriptar incluyendo la extension: ")
+            llave = input("\nIngrese la llave privada para desencriptar el archivo incluyendo la extension: ")
+            decryptionMethodAsymetric(file, llave)
+        except ValueError as e:
+            print(e)
     return True
 if __name__ == '__main__':
     ok = True
